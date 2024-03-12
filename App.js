8dfd08757791ab1920235bb55e907736339db5c9
yun <austru00@gmail.com>
2024-03-12 19:26:56 +0900

    3.4 Paint To Dos

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { theme } from './colors';
 import { useState } from 'react';
 
@@ -14,14 +14,10 @@ export default function App() {
     if (text === ""){
       return;
     }
-    const newToDos = Object.assign(
-      //비어있는 객체
-      {},
-      //이전 객체
-      toDos,
-      //새로운 객체
-      {[Date.now()]:{text, work: working}}
-      )
+      const newToDos = {
+        ...toDos,
+        [Date.now()]: {text, work: working},
+      }
     setToDos(newToDos);
     setText("")
   }
@@ -48,6 +44,13 @@ export default function App() {
       placeholder={working ? "Add a To Do" : "Where do you want to go?"}
       style={styles.input}
       />
+      <ScrollView>
+        {Object.keys(toDos).map((key)=>(
+          <View style={styles.toDos} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -72,7 +75,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 20,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDos:{
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius:15
+  },
+  toDoText: {
+    color:"white",
+    fontSize: 16,
+    fontWeight:"500"
   }
 });
--- colors.js
+++ colors.js
@@ -1,4 +1,5 @@
 export const theme ={
   bg:"black",
-  grey: "#3a3d40"
+  grey: "#3a3d40",
+  toDoBg : "#5C5C60"
 }
